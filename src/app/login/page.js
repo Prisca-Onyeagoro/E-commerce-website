2a9646e5823e22eb603c919d6c35928d9f7bc15a
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-01-12 15:31:40 +0100

    form validation

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,23 @@
+'use client';
 import Layout from '@/components/Layout';
 import React from 'react';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 const LoginScreen = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {};
   return (
     <Layout>
-      <form className="mx-auto max-w-screen-md">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <h1 className="mb-4 text-xl font-bold">Login</h1>
         {/* Email */}
         <div className="mb-4">
@@ -14,10 +26,20 @@ const LoginScreen = () => {
           </label>
           <input
             type="email"
+            {...register('email', {
+              required: 'Please enter your email address',
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: 'Enter a valid eamil',
+              },
+            })}
             className="w-full font-bold"
             id="email"
             autoFocus
           />
+          {errors.email && (
+            <div className="text-red-500 font-bold">{errors.email.message}</div>
+          )}
         </div>
 
         {/* Password */}
@@ -27,10 +49,19 @@ const LoginScreen = () => {
           </label>
           <input
             type="password"
+            {...register('password', {
+              required: 'Please enter a password',
+              minLength: { value: 3, message: 'Password is more than 5 char' },
+            })}
             className="w-full font-bold"
-            id="email"
+            id="password"
             autoFocus
           />
+          {errors.password && (
+            <div className="text-red-500 font-bold ">
+              {errors.password.message}
+            </div>
+          )}
         </div>
 
         <div className="mb-4">
