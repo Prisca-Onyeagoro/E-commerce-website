abe94938453b5ad32fa44a6e80ef78f99cd72034
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-01-15 11:38:20 +0100

    database connection

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,4 +1,5 @@
 import { Mongoose } from 'mongoose';
+import { NextResponse } from 'next/server';
 
 const connection = {};
 
@@ -7,5 +8,9 @@ const connect = async () => {
     const uri = process.env.MONGO_URI;
     Mongoose.connect(uri);
     console.log('connection successful');
-  } catch (error) {}
+    NextResponse.json({ message: 'connection successful' }, { status: 500 });
+  } catch (error) {
+    console.log('connection failed', error.message);
+    NextResponse.json({ message: 'connection successful' }, { status: 500 });
+  }
 };
