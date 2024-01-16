9b074e538b1380f71a8a102b72f0649c8017c893
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-01-16 15:27:23 +0100

    nextauth options

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,7 +1,27 @@
 import NextAuth from 'next-auth';
 
 export const authOptions = {
-  providers: [],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      if (user?.isAdmin) token.isAdmin = user._isAdmin;
+      return token;
+    },
+  },
+  async session({ session, token }) {
+    if (token?._id) session.user._id = token._id;
+    if (token?.isAdmin) session.user.isAdmin = token._isAdmin;
+    return session;
+  },
+  //   providers
+  providers: [
+    CredentialsProvider({
+      async authorize() {},
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
